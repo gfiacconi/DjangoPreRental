531335b56784d5b5a0954e1cd8153f985dc79cae
gfiacconi <fiaccobi@gmail.com>
2022-10-08 18:40:47 +0200

    "beta working navbar"

--- PreRental/PreRental/urls.py
+++ PreRental/PreRental/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("events.urls")),
+    #path("signin/", include('django.contrib.auth.urls')),
+    #path("signin/", include("Login.urls")),
 ]
